0f88100be1e68f6d17ac1c2636f4d13d43bd41f4
Rachel Robell <rachelrobell@Rachel-Robells-MacBook-Pro.local>
2017-10-17 18:33:48 -0700

    Working on querying Interval Tree

--- lib/IntervalTree.py
+++ lib/IntervalTree.py
@@ -70,11 +70,28 @@ Right:
 			Find all intervals in tree that the input interval overlaps with
 
 		Parameters:
-			start: Start of query interval
-			end: End of query interval 
+			interval with one or two points
 
 		Return:
 			List of intervals in tree that overlap
 	"""
-	def findOverlap(start, end):
-		return (True)
+	def querey(interval):
+		ret = set()
+		if (len(interval) == 1 or interval[0] == interval[1]):
+			self._quereyPoint(interval[0], ret)
+		elif (len(interval) == 2):
+			self._quereyRange(interval, ret)
+		else:
+			raise ValueError("Interval Tree Querey takes a list of a maximum of two values ie. ([1] or [1, 2])")
+		return (ret)
+
+	def _quereyPoint(point, ret):
+		left = self.left
+		right = self.right
+		while (left and right)
+		return (ret)
+
+	def _quereyRange(interval, ret):
+		left = self._quereyPoint(interval[0], ret)
+		right = self._quereyPoint(interval[1], ret)
+		return (ret)
